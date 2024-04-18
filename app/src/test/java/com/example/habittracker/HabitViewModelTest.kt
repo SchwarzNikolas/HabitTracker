2357ae20034c7674fc78165b9faf0b39ccce0dcc
Sven <svenarne.lindstroem@gmail.com>
2024-04-18 14:38:47 +0200

    GOOD-29_Sprint2 started testing

---
+++ app/src/test/java/com/example/habittracker/HabitViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.example.habittracker
+
+import org.junit.jupiter.api.Assertions.*
+
+class HabitViewModelTest {
+
+    @org.junit.jupiter.api.Test
+    fun getState() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun onEvent() {
+    }
+}
