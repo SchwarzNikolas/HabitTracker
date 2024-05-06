896b40384d8fc12f4607a06e16fc1d99c7bc04b2
Marta Moroni <marta.moroni0203@stud.hkr.se>
2024-05-06 16:56:59 +0200

    improved code for buttons color

--- app/src/main/java/com/example/habittracker/mood/MoodState.kt
+++ app/src/main/java/com/example/habittracker/mood/MoodState.kt
@@ -6,5 +6,12 @@ import java.time.LocalTime
 
 data class MoodState(
     val moods: List<MoodType> = enumValues<MoodType>().toList(),
-    val selectedMood: MoodType = MoodType.OK
+    val selectedMood: MoodType = MoodType.OK,
+    val moodColors: Map<MoodType, Long> = mapOf(
+        MoodType.BAD to 0xFFFF0000,
+        MoodType.SO_SO to 0xFFFFA500,
+        MoodType.OK to 0xFFFFFF00,
+        MoodType.ALRIGHT to 0xFF00FF00,
+        MoodType.GOOD to 0xFF008000,
+    )
 )
