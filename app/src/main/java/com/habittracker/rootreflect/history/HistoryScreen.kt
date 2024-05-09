c27648d9327d14abca5a8fc3be812161c3676dc3
StackedFrog <isaac.prins4@gmail.com>
2024-05-09 10:15:07 +0200

    added first image :)

--- app/src/main/java/com/habittracker/rootreflect/history/HistoryScreen.kt
+++ app/src/main/java/com/habittracker/rootreflect/history/HistoryScreen.kt
@@ -1,5 +1,7 @@
 package com.habittracker.rootreflect.history
 
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,10 +26,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.habittracker.rootreflect.mood.MoodType
-import java.sql.Date
+import java.io.IOException
 import java.time.LocalDate
 import java.time.Month
-import java.time.YearMonth
+import android.content.Context
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.graphics.drawable.toBitmap
+
 
 @Composable
 fun HistoryScreen(
@@ -36,6 +43,7 @@ fun HistoryScreen(
 ) {
     Column {
         Text(text = "History Screen")
+        Background(onEvent = onEvent, state = state)
         Spacer(modifier = Modifier.weight(1f))
         Row(modifier = Modifier.height(165.dp)) {
             MonthSelector(onEvent = onEvent, state = state)
@@ -111,3 +119,26 @@ fun DailyBox(onEvent: (HistoryEvent) -> Unit, state: HistoryState, dayNum: Int){
     ) {
     }
 }
+@Composable
+fun Background (onEvent: (HistoryEvent) -> Unit, state: HistoryState){
+    val drawable = loadImageFromAssets(LocalContext.current, "images/background.png")
+    val bitmap = drawable?.toBitmap()?.asImageBitmap()
+    Box() {
+        bitmap?.let { BitmapPainter(it) }?.let {
+            Image(
+                painter = it,
+                contentDescription = null)
+        }
+    }
+}
+
+fun loadImageFromAssets(context: Context, filename: String): Drawable? {
+    try {
+        val inputStream = context.assets.open(filename)
+        return Drawable.createFromStream(inputStream, null)
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return null
+}
+
